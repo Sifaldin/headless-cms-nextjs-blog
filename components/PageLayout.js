0d2d0d7bae86e56abef92aa9e19cce9fcb1b43ab
Sifaldin <saif.m.abbas@gmail.com>
2021-01-28 19:12:48 +0100

    add components - add structure - add styling

---
+++ components/PageLayout.js
@@ -0,0 +1,22 @@
+import NavbarCmp from 'components/NavbarCmp';
+import { Container } from 'react-bootstrap'
+
+
+export default function PageLayout({ children, className }) {
+  return (
+    <Container>
+      <NavbarCmp />
+      <div className={`page-wrapper ${className}`}>
+        {children}
+      </div>
+      <footer className="page-footer">
+        <div>
+          <a href="#">courses</a>{' | '}
+          <a href="#">github</a>{' | '}
+          <a href="#">facebook</a>
+        </div>
+      </footer>
+    </Container>
+
+  )
+}
