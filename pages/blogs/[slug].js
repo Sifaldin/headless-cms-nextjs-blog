473db893a85728086f29272dfb3128850fddef31
Sifaldin <saif.m.abbas@gmail.com>
2021-02-03 09:17:56 +0100

    add content - add code block content

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -2,15 +2,19 @@ import BlogHeader from 'components/BlogHeader';
 import PageLayout from 'components/PageLayout'
 import { getAllBlogs, getBlogBySlug } from 'lib/api';
 import { Row, Col } from 'react-bootstrap';
+import BlockContent from '@sanity/block-content-to-react';
+import BlockContentCmp from 'components/BlockContentCmp';
+
+
 
 const BlogDetail = ({ blog }) => {
   return (
     <PageLayout className="blog-detail-page">
       <Row>
         <Col md={{ span: 10, offset: 1 }}>
-         <BlogHeader blog={blog} />
+          <BlogHeader blog={blog} />
           <hr />
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          <BlockContentCmp blog={blog} />
         </Col>
       </Row>
     </PageLayout>
